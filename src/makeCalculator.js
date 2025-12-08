bce8c20f71e0928039ba1c77779a4d75a2a1c91c
Nazar Kundys <nazarrisd@gmail.com>
2025-12-08 22:23:52 +0200

    feat: ✨ implemented calculator functionality

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    add(arg) {
+      this.result += arg;
+
+      return this;
+    },
+    subtract(arg) {
+      this.result -= arg;
+
+      return this;
+    },
+    multiply(arg) {
+      this.result *= arg;
+
+      return this;
+    },
+    divide(arg) {
+      this.result /= arg;
+
+      return this;
+    },
+    reset(arg) {
+      this.result = 0;
+
+      return this;
+    },
+    operate(operation, arg) {
+      operation.call(this, arg);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
